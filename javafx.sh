2c14f17c75cffd0eb608ca63716cf325ff9f5465
Christian <christian@edv-ehm.de>
2023-06-15 18:59:01 +0200

    Skript für JavaFX

---
+++ javafx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ./lib/
+ln -s /usr/share/java/javafx-base-11.jar
+ln -s /usr/share/java/javafx-controls-11.jar
+ln -s /usr/share/java/javafx-fxml-11.jar
+ln -s /usr/share/java/javafx-graphics-11.jar
+ln -s /usr/share/java/javafx-media-11.jar
+ln -s /usr/share/java/javafx-swing-11.jar
+ln -s /usr/share/java/javafx-web-11.jar
+
